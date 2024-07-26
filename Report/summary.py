29fa6852403b72b11d5bd7c5ccc98c7c6e230763
Bhanu <mamillapallibhanu1@gmail.com>
2024-07-26 16:18:49 -0400

    renamed:    Report/Final_Test_Dataset.xlsx -> Report/results/GNN_vs_FFN_Baseline.xlsx

--- Report/summary.py
+++ Report/summary.py
@@ -9,8 +9,9 @@ truths = true_df['target']
 T_labels = true_df['T']
 
 
-datasets = ['cond']
-splits = ['0.1','0.2','0.3','0.4', '0.5']
+datasets = ['cond_transfer_from_vd']
+splits = ['0.1', '0.2']
+#splits = ['0.1','0.2','0.3','0.4', '0.5','0.6','0.7']
 T_index = ['1', '2', '3', '4', '5']
 results_dict = dict()
 
